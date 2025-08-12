d37ca849d4cba27aa12af119a4ca324866784ca9
DavidP <escontrela@live.com>
2025-08-12 12:55:18 +0200

    Add new PGN panel whith tactics

--- src/main/java/com/davidp/chessjourney/application/ui/controls/PGNEditorController.java
+++ src/main/java/com/davidp/chessjourney/application/ui/controls/PGNEditorController.java
@@ -1,6 +1,7 @@
 package com.davidp.chessjourney.application.ui.controls;
 
 import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +11,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 
+/**
+ * This controller is responsible for the PGN editor control that allows users to edit and copy FEN
+ * and PGN strings.
+ */
 public class PGNEditorController extends Pane {
 
   @FXML private Pane rootPane;
@@ -50,8 +55,56 @@ public class PGNEditorController extends Pane {
   private void initialize() {}
 
   @FXML
-  void buttonAction(ActionEvent event) {}
+  void buttonAction(ActionEvent event) {
+
+    if (isButtonCloseClicked(event)) {
+
+      // TODO event handler should be notified to close the PGN editor
+
+    } else if (isTxtFenPressed(event)) {
+
+      txtFen.copy();
+    } else if (idPGNCopyButtonClicked(event)) {
+
+      txtPGN.copy();
+    }
+  }
 
   @FXML
-  void handleKeyPress(KeyEvent event) {}
+  void handleKeyPress(KeyEvent event) {
+
+    if (isFenInputField(event)) {
+      // TODO we should notify the event handler that the FEN has changed
+    }
+
+    if (isPGNInputField(event)) {
+
+      // TODO we should notify the event handler that the PGN has changed
+    }
+  }
+
+  private boolean isPGNInputField(KeyEvent event) {
+
+    return event.getSource() == txtPGN;
+  }
+
+  private boolean isFenInputField(KeyEvent event) {
+
+    return event.getSource() == txtFen;
+  }
+
+  private boolean idPGNCopyButtonClicked(ActionEvent event) {
+
+    return event.getSource() == btnCopyPGN;
+  }
+
+  private boolean isTxtFenPressed(ActionEvent event) {
+
+    return event.getSource() == btnCopyFen;
+  }
+
+  private boolean isButtonCloseClicked(ActionEvent event) {
+
+    return event.getSource() == btCloseCtrlPGN;
+  }
 }
